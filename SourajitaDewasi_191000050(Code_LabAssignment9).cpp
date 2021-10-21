5adc106beeaa96bd4796e3f7c83edf2697f9f408
Sourajita Dewasi <89794866+SourajitaDewasi@users.noreply.github.com>
2021-10-21 15:32:48 +0530

    Add files via upload
    This code has references from : https://www.educative.io/edpresso/how-to-implement-the-des-algorithm-in-cpp

---
+++ SourajitaDewasi_191000050(Code_LabAssignment9).cpp
@@ -0,0 +1,271 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+string round_keys[16];
+string pt;
+
+string XOR(string a, string b){ 
+	string result = ""; 
+	int size = b.size();
+	for(int i = 0; i < size; i++){ 
+		if(a[i] != b[i]){ 
+			result += "1"; 
+		}
+		else{ 
+			result += "0"; 
+		} 
+	} 
+	return result; 
+} 
+
+string Decimal2Binary(int dec)
+    {string bin;
+    while(dec != 0) {
+		bin = (dec % 2 == 0 ? "0" : "1") + bin; 
+		dec = dec/2;
+	}
+	while(bin.length() < 4){
+		bin= "0" + bin;
+	}
+    return bin;
+}
+
+int Binary2Decimal(string bin)
+{   int count = 0;
+    int dec = 0;
+	int size = bin.length();
+	for(int i = size-1; i >= 0; i--)
+	{
+    	if(bin[i] == '1'){
+        	dec += pow(2, count);
+    	}
+    count++;
+	}
+	return dec;
+}
+
+
+string shiftLeft(string key_chunk){ 
+    string shifted="";  
+        for(int i = 1; i < 28; i++){ 
+            shifted += key_chunk[i]; 
+        } 
+        shifted += key_chunk[0];   
+    return shifted; 
+} 
+// Function to do a circular left shift by 2
+string shiftLeftAgain(string key_chunk){ 
+    string shifted=""; 
+    for(int i = 0; i < 2; i++){ 
+        for(int j = 1; j < 28; j++){ 
+            shifted += key_chunk[j]; 
+        } 
+        shifted += key_chunk[0]; 
+        key_chunk= shifted; 
+        shifted =""; 
+    } 
+    return key_chunk; 
+}
+
+void generate_keys(string key){
+	
+	int pc1[56] = {
+	57,49,41,33,25,17,9, 
+	1,58,50,42,34,26,18, 
+	10,2,59,51,43,35,27, 
+	19,11,3,60,52,44,36,		 
+	63,55,47,39,31,23,15, 
+	7,62,54,46,38,30,22, 
+	14,6,61,53,45,37,29, 
+	21,13,5,28,20,12,4 
+	};
+	
+	int pc2[48] = { 
+	14,17,11,24,1,5, 
+	3,28,15,6,21,10, 
+	23,19,12,4,26,8, 
+	16,7,27,20,13,2, 
+	41,52,31,37,47,55, 
+	30,40,51,45,33,48, 
+	44,49,39,56,34,53, 
+	46,42,50,36,29,32 
+	}; 
+	
+	string perm_key =""; 
+	for(int i = 0; i < 56; i++){ 
+		perm_key+= key[pc1[i]-1]; 
+	} 
+	string left= perm_key.substr(0, 28); 
+	string right= perm_key.substr(28, 28); 
+	for(int i=0; i<16; i++){ 
+		if(i == 0 || i == 1 || i==8 || i==15 ){
+			left= shiftLeft(left); 
+			right= shiftLeft(right);
+		} 
+		else{
+			left= shiftLeftAgain(left); 
+			right= shiftLeftAgain(right);
+		}
+		string combined_key = left + right;	string round_key = ""; 
+		for(int i = 0; i < 48; i++){ 
+			round_key += combined_key[pc2[i]-1]; 
+		}   
+		round_keys[i] = round_key; 
+	} 
+}
+
+string DES(){ 
+	int initial_permutation[64] = { 
+	58,50,42,34,26,18,10,2, 
+	60,52,44,36,28,20,12,4, 
+	62,54,46,38,30,22,14,6, 
+	64,56,48,40,32,24,16,8, 
+	57,49,41,33,25,17,9,1, 
+	59,51,43,35,27,19,11,3, 
+	61,53,45,37,29,21,13,5, 
+	63,55,47,39,31,23,15,7 
+	}; 
+	
+	int expansion_table[48] = { 
+	32,1,2,3,4,5,4,5, 
+	6,7,8,9,8,9,10,11, 
+	12,13,12,13,14,15,16,17, 
+	16,17,18,19,20,21,20,21, 
+	22,23,24,25,24,25,26,27, 
+	28,29,28,29,30,31,32,1 
+	}; 
+	
+	int substition_boxes[8][4][16]=  
+	{{ 
+        14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7, 
+        0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8, 
+        4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0, 
+        15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13 
+    }, 
+    { 
+        15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10, 
+        3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5, 
+        0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15, 
+        13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9 
+    }, 
+    { 
+        10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8, 
+        13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1, 
+        13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7, 
+        1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12 
+    }, 
+    { 
+        7,13,14,3,0,6,9,10,1,2,8,5,11,12,4,15, 
+        13,8,11,5,6,15,0,3,4,7,2,12,1,10,14,9, 
+        10,6,9,0,12,11,7,13,15,1,3,14,5,2,8,4, 
+        3,15,0,6,10,1,13,8,9,4,5,11,12,7,2,14 
+    }, 
+    { 
+        2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9, 
+        14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6, 
+        4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14, 
+        11,8,12,7,1,14,2,13,6,15,0,9,10,4,5,3 
+    }, 
+    { 
+        12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11, 
+        10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8, 
+        9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6, 
+        4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13 
+    }, 
+    { 
+        4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1, 
+        13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6, 
+        1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2, 
+        6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12 
+    }, 
+    { 
+        13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7, 
+        1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2, 
+        7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8, 
+        2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11 
+    }};
+	
+	int permutation_tab[32] = { 
+	16,7,20,21,29,12,28,17, 
+	1,15,23,26,5,18,31,10, 
+	2,8,24,14,32,27,3,9,
+	19,13,30,6,22,11,4,25 
+	}; 
+	
+	int inverse_permutation[64]= { 
+	40,8,48,16,56,24,64,32, 
+	39,7,47,15,55,23,63,31, 
+	38,6,46,14,54,22,62,30, 
+	37,5,45,13,53,21,61,29, 
+	36,4,44,12,52,20,60,28, 
+	35,3,43,11,51,19,59,27, 
+	34,2,42,10,50,18,58,26, 
+	33,1,41,9,49,17,57,25 
+	};
+    int count=0;
+  	string perm = ""; cout<<"\n Initial Permutation: ";
+	for(int i = 0; i < 64; i++){ 
+		perm += pt[initial_permutation[i]-1]; 
+	}
+	cout<<"\n Splitting Inti Substrings ";
+	string left = perm.substr(0, 32); 
+	string right = perm.substr(32, 32);
+	
+	for(int i=0; i<16; i++) { 
+    	string right_expanded = ""; 
+    	for(int i = 0; i < 48; i++) { 
+      		right_expanded += right[expansion_table[i]-1]; 
+    };  if(count%2 !=0 )
+        cout<<"\n XOR keys with right half of string";
+		string xored = XOR(round_keys[i], right_expanded);  
+		
+		string res = ""; 
+		if(count%2 !=0 )
+		cout<<"\n Using Substitution Box "; count++;
+		for(int i=0;i<8; i++){ 	string row1= xored.substr(i*6,1) + xored.substr(i*6 + 5,1);
+      		int row = Binary2Decimal(row1);
+      		string col1 = xored.substr(i*6 + 1,1) + xored.substr(i*6 + 2,1) + xored.substr(i*6 + 3,1) + xored.substr(i*6 + 4,1);;
+			int col = Binary2Decimal(col1);
+			int val = substition_boxes[i][row][col];
+			res += Decimal2Binary(val);  
+		} 
+		
+		string perm2 =""; 
+		for(int i = 0; i < 32; i++){ 
+			perm2 += res[permutation_tab[i]-1]; 
+		}
+		xored = XOR(perm2, left);
+		left = xored; 
+		if(i < 15){ 
+			string temp = right;
+			right = xored;
+			left = temp;
+		} 
+	} 	string combined_text = left + right;   
+	string ciphertext =""; 
+
+	for(int i = 0; i < 64; i++){ 
+		ciphertext+= combined_text[inverse_permutation[i]-1]; 
+	}
+	return ciphertext; 
+}
+int main(){ 
+    string pt= "1010101111001101111001101010101111001101000100110010010100110110";
+	string key= "1111110011100011100111000111000011100001110001110000000";
+	cout<<"\n Key: "<<key;
+    generate_keys(key);
+    cout<<"\n Plain text: "<<pt; 
+    string ct= DES(); 
+    cout<<"\n Ciphertext: "<<ct;
+} 
